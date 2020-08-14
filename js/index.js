29f0402280b4981ce3eb4260f7d6f3cf96972805
Kender Mendoza <kendermendoza591@outlook.com>
2020-08-13 21:29:55 -0600

    Feature copy

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const container = document.getElementById('container')
 const btnAdd = document.getElementById('add')
 const textArea = document.getElementById('text-area')
+const copyLinks = document.getElementById('copy-links')
 
 btnAdd.addEventListener('click', () => {
     const selectorId = createIdToSelector()
@@ -8,4 +9,18 @@ btnAdd.addEventListener('click', () => {
     container.insertAdjacentHTML('beforeend', selector)
     const elements = selectElements(selectorId)
     addEventToSelector(elements)
+})
+
+copyLinks.addEventListener('click',()=>{
+    let selection = window.getSelection()
+    
+    let range = document.createRange()
+    range.selectNode(textArea)
+    selection.removeAllRanges(range)
+    selection.addRange(range)
+    document.execCommand('copy')
+    
+    const textAreaLinks = document.getElementsByClassName('text-area-link')
+    for (const link of textAreaLinks) link.classList.add('animate')
+    setTimeout(()=>{for (const link of textAreaLinks) link.classList.remove('animate')},1000)
 })
